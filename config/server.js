0898c021d4b7ce4c52fa0ae7802b127d5e6fa6ba
saritaborad <borad.sarita@gmail.com>
2023-07-11 09:43:27 +0530

    env production folder added

--- config/server.js
+++ config/server.js
@@ -1,11 +1,18 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "127.0.0.1"),
   port: env.int("PORT", 1337),
-  url: process.env.URL,
   app: {
     keys: env.array("APP_KEYS"),
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+});
+
+module.exports = ({ env }) => ({
+  proxy: true,
+  url: env("HOST_URL"), // Sets the public URL of the application.
+  app: {
+    keys: env.array("APP_KEYS"),
   },
 });
